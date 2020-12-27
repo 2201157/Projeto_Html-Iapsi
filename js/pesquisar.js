3c89417bd6390ef0a1e923e124356fdbc53f283b
sandro pinto <2201146@my.ipleira.pt>
2020-12-27 14:59:00 +0000

    Adicionado ficheiro js para pesquisa de musicas

--- js/pesquisar.js
+++ js/pesquisar.js
@@ -2,34 +2,48 @@
 
 'use strict'
 
-var cloneMedia = $('.media').clone();
 
 
+$('#pesquisa').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)  // the enter key code
+  {
+    $('#btSearch').trigger('click');
+  }
+});   
 
 $('#btSearch').on('click', function(){
 	console.log('passou');
+    
+    location.href = "#nome_artista_api";
     var valorPesquisa = $('#pesquisa').val();
  $('.panel-title').text('Resultados da pesquisa para"'
  + valorPesquisa + '"')
-    $('.media-list').html('');
+    
 
     $.ajax({
         method: "GET",
-        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + valorPesquisa +"&api_key=cb5873017d844ca90043a3eae82f9055&r=json"
+        url: "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + valorPesquisa +"&api_key=cb5873017d844ca90043a3eae82f9055&format=json"
     }).done(function(artist){
-        console.log('artist');
-        console.log(artist);
+        //console.log('artist');
+        //console.log(artist);
 
-        artist.Search.function(artist)
-            console.log
-            var liMedia = cloneMedia.clone();
-            $('#image', liMedia).attr("src", artist.getImages);
-            $('.title', liMedia).text(artist.getInfo);
-            //$('.ano', liMedia).text(result.published);
-            //$('.tipo', liMedia).text(result.summary);
+        $.each(artist, function(key, value){
+          //$("div").append(field + " ");
+          /*console.log("KEY : " + key)
+          console.log("VALUE : " + value.mbid)
+          console.log("IMAGEM : ")
+          console.log(value.image[0]["#text"])
+          console.log("LINK ")
+          console.log(value.bio.links.link.href)*/
+          console.log(value)
+$('#nome_artista_api').html(value.name);
+$('#bio_artista_api').html(value.bio.content);
+$('#imagens_artista_api').html('<img src="' + value.image[3]["#text"] + '""/>');
 
-            $('.media-list').append(liMedia);
+        });
 
         
+    
     })
 });
---
+++ js/pesquisar_musicas.js
@@ -0,0 +1,41 @@
+'use strict'
+
+
+
+$('#pesquisa').keypress(function (e) {
+ var key = e.which;
+ if(key == 13)  // the enter key code
+  {
+    $('#btSearch').trigger('click');
+  }
+});   
+
+$('#btSearch').on('click', function(){
+
+    
+    location.href = "#nome_artista_api";
+    var valorPesquisa = $('#pesquisa').val();
+ $('.panel-title').text('Resultados da pesquisa para"'
+ + valorPesquisa + '"')
+    
+
+    $.ajax({
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=track.search&track=" + valorPesquisa +"&api_key=cb5873017d844ca90043a3eae82f9055&limit=5&format=json"
+    }).done(function(music){
+       
+
+        $.each(music, function(key, value){
+    
+          console.log(value.trackmatches.track[0].name + value.trackmatches.track[0].artist)
+          console.log(value.trackmatches.track[1].name + value.trackmatches.track[1].artist)
+          console.log(value.trackmatches.track[2].name + value.trackmatches.track[2].artist)
+          console.log(value.trackmatches.track[3].name + value.trackmatches.track[3].artist)
+          console.log(value.trackmatches.track[4].name + value.trackmatches.track[4].artist)
+
+        });
+
+        
+    
+    })
+});
