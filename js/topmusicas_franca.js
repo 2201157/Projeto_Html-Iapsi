f4b373b4c2ed9420cda2d58072e29b073f0bf5f5
Rodrigo Peres <2201157@my.ipleiria.pt>
2021-01-15 14:32:59 +0000

    Add fotos de albuns no top musicas

--- js/topmusicas_franca.js
+++ js/topmusicas_franca.js
@@ -1,21 +1,48 @@
+'use strict'
+
+var callback = function(img,i) {
+
+    img =  img.replace(/"/g, "");
+    console.log(img)
+    //console.log(i)
+     
+    $('#img'+i).attr("src",img);
+   //$('#img'+i).html('<img src="' + img + '""/>');
+}
+
+
+
 $(document).ready(function(){
 
     $.ajax({
+
         method: "GET",
         url: "http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=france&api_key=cb5873017d844ca90043a3eae82f9055&format=json"
     }).done(function(country){
         
-            var i = 0
+        var i = 0
+        var img ;
+        var img_2;
 
         $.each(country, function(key, value){
 
+            
       
-            for(i = 0; i <=9; i++){
+                for(i = 0; i <=9; i++){
 
-        
-        $('#img'+i).html('<img src="' + value.track[i].image[1]['#text'] + '""/>');
+                    img = JSON.stringify(value.track[i].name)
+                    img_2 =  img.replace(/ /g, "+");
+                    img = album_top(img_2,i)
+                   
+                    
+
+                    
+
+                    
         
         }
+        
+        
         
         
 
@@ -28,3 +55,33 @@ $(document).ready(function(){
     })
 
 })
+
+function album_top(musica,i){
+
+   // console.log(musica)
+
+    var img_url
+
+$.ajax({
+        method: "GET",
+        url: "http://ws.audioscrobbler.com/2.0/?method=album.search&album="+musica+"&api_key=cb5873017d844ca90043a3eae82f9055&format=json"
+    }).then(function(url){
+
+        
+
+
+        $.each(url, function(key, value){
+
+            img_url  = JSON.stringify(value.albummatches.album[0].image[2]['#text'])
+
+           
+            callback(img_url,i)
+               
+
+
+        })
+
+
+
+    })
+}
